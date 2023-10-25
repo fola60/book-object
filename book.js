8e3fdd27709d96e9ef20b46ec004b8852ed92305
fola60 <afolabiadekanle@gmail.com>
2023-10-25 22:18:32 +0100

    --all

--- book.js
+++ book.js
@@ -15,17 +15,36 @@ function book(title,author,pages,read){
          }
      }
 }
-//bla bla
-function blal()
+//const creareCardBtn = document.getElementById('cBook')
+// const cards = document.getElementById('cards');
+// let i = 0;
+// function createCard(){
+//     for (books in myLibrary){
+//         const card = document.createElement('div');
+//         card.textContent = `Bookname: ${myLibrary[i].title} Bookauthor:${myLibrary[0].author} BookPages: ${myLibrary[0].pages}`
+//         cards.append(card);
+//        i++;
+//     }
+// }
+const htmlElement = document.querySelector("html");
+htmlElement.style.filter = "brightness(65%)"
+let userBook;
+let bookName;
+let bookAuthor;
+let bookPages;
+let readOrNot;
 function addBookToLibrary(){
-  const bookName = prompt("What is the name of the book?");
-  const bookAuthor = prompt("Who Wrote the book>");
-  const bookPages = int(prompt("How many pages are there"));
-  const readOrNot = prompt("Have you read the book or not,say True if you have and False if you havent.");
+  let bookName = prompt("What is the name of the book?");
+  let bookAuthor = prompt("Who Wrote the book");
+  let bookPages = prompt("How many pages are there");
+  let readOrNot = prompt("Have you read the book or not,say True if you have and False if you havent.");
 
-  const newBook1 = new book(bookName,bookAuthor,bookPages,readOrNot)
-  myLibrary.push(newBook1);
+  let userBook = new book([bookName],[bookAuthor],[bookPages],[readOrNot])
+  myLibrary.push(userBook);
+  
+  createCard();
 }
-addBookToLibrary();
+creareCardBtn.addEventListener('click', addBookToLibrary);
+
 // const onePiece = new book("One piece", "Eichiro oda", '20000',true);
 // onePiece.info();
